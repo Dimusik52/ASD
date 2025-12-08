8809a918530005e2a9498fa0cc38386f6baeb291
Dmitry Pikhulya <volodya.pikhulya@mail.ru>
2025-12-08 23:07:48 +0300

    Fixed StackList

--- lib_stacklist/stacklist.h
+++ lib_stacklist/stacklist.h
@@ -31,45 +31,39 @@ StackList<T>::StackList() {
 template <class T>
 StackList<T>::StackList(List<T>& other) {
   for (auto it = other.begin(); it != other.end(); ++it) {
-    _data.push_back(*it);
+    _data.push_front(*it);
   }
 }
 
 template <class T>
 StackList<T>::StackList(std::initializer_list<T> init_list) {
   for (const auto& item : init_list) {
-    _data.push_back(item); // тут
+    _data.push_front(item);
   }
 }
 template <class T>
 StackList<T>::StackList(StackList<T>& other) {
   for (auto it = other._data.begin(); it != other._data.end(); ++it) {
-    _data.push_back(*it); // тут
+    _data.push_back(*it);
   }
 }
 
 template <class T>
 void StackList<T>::push(T val) {
   if (is_full()) throw std::logic_error("push(): Stack is full!");
-  _data.push_back(val); // push_front()
+  _data.push_front(val);
 }
 
 template <class T>
 void StackList<T>::pop() {
   if (is_empty()) throw std::logic_error("pop(): Stack is empty!");
-  _data.pop_back(); // сделать через pop_front чтобы сложность была O(1)
+  _data.pop_front();
 }
 
 template <class T>
 T StackList<T>::top() {
   if (is_empty()) throw std::logic_error("top(): Stack is empty!");
-  auto it = _data.begin();
-  auto prev = it;
-  while (it != _data.end()) {
-    prev = it;
-    ++it;
-  }
-  return *prev; // возращать _head;
+  return _data.begin().get_node()->value;
 }
 
 template <class T>
