4ebf1be4b2af53b2e896f96f30c3b783b023b7ae
Dmitry Pikhulya <volodya.pikhulya@mail.ru>
2026-03-01 22:36:30 +0300

    Created tests for UnsortedTableM class.

---
+++ tests/test_unsortedtablem.cpp
@@ -0,0 +1,68 @@
+// Copyright (c) 2026 Dmitriy Pikhulya 3824B1PR2
+
+#include <gtest/gtest.h>
+#include "../lib_unsortedtablem/unsortedtablem.h"
+
+TEST(UnsortedTableTest, InsertAndFind) {
+  UnsortedTableM<int, std::string> table;
+
+  table.insert(1, "one");
+  table.insert(2, "two");
+  table.insert(3, "three");
+
+  auto* val = table.find(2);
+  ASSERT_NE(val, nullptr);
+  EXPECT_EQ(*val, "two");
+
+  EXPECT_EQ(table.find(4), nullptr);
+}
+
+TEST(UnsortedTableTest, UpdateExistingKey) {
+  UnsortedTableM<int, std::string> table;
+
+  table.insert(1, "one");
+  table.insert(1, "ONE");
+
+  auto* val = table.find(1);
+  ASSERT_NE(val, nullptr);
+  EXPECT_EQ(*val, "ONE");
+}
+
+TEST(UnsortedTableTest, Erase) {
+  UnsortedTableM<int, std::string> table;
+
+  table.insert(1, "one");
+  table.insert(2, "two");
+
+  table.erase(1);
+
+  EXPECT_EQ(table.find(1), nullptr);
+  EXPECT_NE(table.find(2), nullptr);
+
+  table.erase(999);
+}
+
+TEST(UnsortedTableTest, IsEmpty) {
+  UnsortedTableM<int, std::string> table;
+
+  EXPECT_TRUE(table.isEmpty());
+
+  table.insert(1, "one");
+  EXPECT_FALSE(table.isEmpty());
+
+  table.erase(1);
+  EXPECT_TRUE(table.isEmpty());
+}
+
+TEST(UnsortedTableTest, ModifyThroughPointer) {
+  UnsortedTableM<int, std::string> table;
+  table.insert(1, "one");
+
+  auto* val = table.find(1);
+  ASSERT_NE(val, nullptr);
+
+  *val = "modified";
+
+  auto* check = table.find(1);
+  EXPECT_EQ(*check, "modified");
+}
