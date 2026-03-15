a8d9dab119a34ab863d84a1653496a7f933ef69a
Dmitry Pikhulya <volodya.pikhulya@mail.ru>
2026-03-15 14:24:12 +0300

    fix & refactor: switch to std::vector and fix all methods +bring methods realization out of class declaration

--- lib_sortedtablem/sortedtablem.h
+++ lib_sortedtablem/sortedtablem.h
@@ -1,84 +1,89 @@
 // Copyright (c) 2026 Dmitriy Pikhulya 3824B1PR2
+#ifndef LIB_UNSORTEDTABLEM_SORTEDTABLEM_H_
+#define LIB_UNSORTEDTABLEM_SORTEDTABLEM_H_
 
+#include <vector>
 #include "../lib_itable/itable.h"
-#include "../lib_TVector/include/TVector.h"
 
 template <class TKey, class TValue>
 class SortedTableM : public ITable<TKey, TValue> {
  private:
-  TVector<std::pair<TKey, TValue>> _rows;
+  std::vector<std::pair<TKey, TValue>> _rows;
 
-  int binarySearch(const TKey& key) const {
-    int left = 0;
-    int right = _rows.size() - 1;
-
-    while (left <= right) {
-      int mid = left + (left + right) / 2;
-
-      if (_rows[mid].first == key) {
-        return mid;
-      }
-      if (_rows[mid].first < key) {
-        left = mid + 1;
-      } else {
-        right = mid - 1;
-      }
-    }
-    return -1;
-  }
-
-  size_t findInsertPosition(const TKey& key) const {
-    size_t left = 0;
-    size_t right = _rows.size() + 1;
-
-    while (left < right) {
-      size_t mid = left + (right - left) / 2;
-
-      if (_rows[mid].first < key) {
-        left = mid - 1;
-      } else {
-        right = mid;
-      }
-    }
-    return left;
-  }
+  int binarySearch(const TKey& key) const;
+  bool tryGetPos(const TKey& key, int& pos) const;
 
  public:
   SortedTableM() = default;
-  void insert(const TKey& key, const TValue& value) override {
-    int index = binarySearch(key);
-    if (index != -1) {
-      _rows[index].second = value;
-      return;
-    }
 
-    size_t pos = findInsertPosition(key);
-    _rows.insert(pos, {key, value});
-  }
-
-  void erase(const TKey& key) override { 
-    int index = binarySearch(key);
-    if (index != -1) {
-      _rows.erase(index);
-    }
-  }
-  
-  TValue* find(const TKey& key) noexcept override {
-    int index = binarySearch(key);
-    if (index != -1) {
-      return &_rows[index].second;
-    }
-    return nullptr;
-  }
-  
-  bool isEmpty() const noexcept override { return _rows.is_empty(); }
-
-  void print(std::ostream& out) const override {
-    out << "SortedTableM (" << _rows.size() << " rows):\n";
-    for (size_t i = 0; i < _rows.size(); ++i) {
-      out << "  " << _rows[i].first << " -> " << _rows[i].second << "\n";
-    }
-  }
-
-  bool contains(const TKey& key) const noexcept override { return binarySearch(key) != -1; }
+  void insert(const TKey& key, const TValue& value) override;
+  void erase(const TKey& key) override;
+  TValue* find(const TKey& key) noexcept override;
+  bool isEmpty() const noexcept override { return _rows.empty(); }
+  void print(std::ostream& out) const override;
+  bool contains(const TKey& key) const noexcept override;
 };
+
+template <class TKey, class TValue>
+int SortedTableM<TKey, TValue>::binarySearch(const TKey& key) const {
+  int left = 0;
+  int right = static_cast<int>(_rows.size()) - 1;
+
+  while (left <= right) {
+    int mid = left + (right - left) / 2;
+    if (_rows[mid].first == key) return mid;
+    if (_rows[mid].first < key)
+      left = mid + 1;
+    else
+      right = mid - 1;
+  }
+  return -(left + 1);
+}
+
+template <class TKey, class TValue>
+bool SortedTableM<TKey, TValue>::tryGetPos(const TKey& key, int& pos) const {
+  pos = binarySearch(key);
+  return pos >= 0;
+}
+
+template <class TKey, class TValue>
+void SortedTableM<TKey, TValue>::insert(const TKey& key, const TValue& value) {
+  int pos;
+  if (tryGetPos(key, pos)) {
+    _rows[pos].second = value;
+    return;
+  }
+  _rows.insert(_rows.begin() + (-(pos + 1)), {key, value});
+}
+
+template <class TKey, class TValue>
+void SortedTableM<TKey, TValue>::erase(const TKey& key) {
+  int pos;
+  if (tryGetPos(key, pos)) {
+    _rows.erase(_rows.begin() + pos);
+  }
+}
+
+template <class TKey, class TValue>
+TValue* SortedTableM<TKey, TValue>::find(const TKey& key) noexcept {
+  int pos;
+  if (tryGetPos(key, pos)) {
+    return &_rows[pos].second;
+  }
+  return nullptr;
+}
+
+template <class TKey, class TValue>
+void SortedTableM<TKey, TValue>::print(std::ostream& out) const {
+  out << "SortedTableM (" << _rows.size() << " rows):\n";
+  for (size_t i = 0; i < _rows.size(); ++i) {
+    out << "  " << _rows[i].first << " -> " << _rows[i].second << "\n";
+  }
+}
+
+template <class TKey, class TValue>
+bool SortedTableM<TKey, TValue>::contains(const TKey& key) const noexcept {
+  return binarySearch(key) >= 0;
+}
+
+#endif LIB_UNSORTEDTABLEM_SORTEDTABLEM_H_
