94b90c41fdcb98f5a6eda01e0dc01fd1d7a75dfa
Dmitry Pikhulya <volodya.pikhulya@mail.ru>
2026-06-06 22:23:14 +0300

    test: add new test

--- tests/test_rbtree.cpp
+++ tests/test_rbtree.cpp
@@ -445,3 +445,22 @@ TEST(RBTreeTest, DuplicateKeysUpdateValue) {
 
   EXPECT_TRUE(tree.isRBTree());
 }
+
+TEST(RBTreeTest, ColorAlternationAfterRotations1) {
+  RBTree<int, std::string> tree;
+
+  tree.insert(50, "50");
+  tree.insert(30, "30");
+  tree.insert(80, "80");
+  tree.insert(10, "10");
+  tree.insert(40, "40");
+  tree.insert(70, "70");
+  tree.insert(90, "90");
+  tree.insert(5, "5");
+  tree.insert(20, "20");
+  tree.insert(7, "7");
+
+  tree.printTree();
+
+  EXPECT_TRUE(tree.isRBTree());
+}
